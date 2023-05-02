5c9d007ef8df59d909a8c5d5c6243a8c07f89d6b
Tony Fadayomi <119269359+CyberFvde@users.noreply.github.com>
2023-05-02 12:07:37 -0500

    Create AzureFunctions.py

---
+++ AzureFunctions.py
@@ -0,0 +1,14 @@
+import azure.functions as func
+import logging
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        return func.HttpResponse(
+             "Please pass a name on the query string or in the request body",
+             status_code=400
+        )
+
+    return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
